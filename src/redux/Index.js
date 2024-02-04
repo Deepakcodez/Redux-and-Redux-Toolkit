9731930e0f48fc3caadd740aa429047d35c89577
Deepak <deepak2603dk@gmail.com>
2024-02-04 18:39:41 +0530

    redux tookkit

--- src/redux/Index.js
+++ src/redux/Index.js
@@ -1,14 +1,18 @@
+import axios from "axios";
 import { createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
 import logger from "redux-logger";
+ //thunk used in asyn function it gives fun two parameter dispatch and getstate
 
 // action const
+const  init = "init"
 const  increment = "increment"
 const  decrement = "decrement"
 const  incrementByAmount = "incrementByAmount"
 
 //store 
 //logger is a middleware which is show the prev state ,action, next state *optional
-const store = createStore(reducer,applyMiddleware(logger.default))
+const store = createStore(reducer,applyMiddleware(thunk,logger))
 
 
 
@@ -23,12 +27,31 @@ function reducer(state={amount:5},action) {
             return{amount:state.amount - 1}    
 
         case incrementByAmount:
+            return{amount:state.amount + action.payload}
+
+        case init:
             return{amount:state.amount + action.payload}    
             
         default:
             return state
     }
 }
+
+
+
+
+
+//async API call
+
+async function initUser(dispatch,getState){
+    const {data} = await axios.get("http://localhost:3000/accounts/1")
+    dispatch({type:init, payload:data.amount})
+}
+
+
+
+
+
 
 
 
@@ -46,6 +69,10 @@ function incByAmount(value) {
     return { type : incrementByAmount , payload:value }
     
 }
+function initialise(value) {
+    return { type : init , payload:value }
+    
+}
 
 
 
@@ -55,7 +82,9 @@ store.dispatch(
     // {type : 'decrement'}
     // {type : "incrementByAmount", payload:5 }
     // inc()
-    incByAmount(4)
+    // incByAmount(4)
+    // initialise(5)
+    initUser
 
     )
     
@@ -69,3 +98,9 @@ store.dispatch(
 
 
  })
+
+
+
+
+ //for db server:-  npm i json-server
+ // json-server db.json   in terminal
